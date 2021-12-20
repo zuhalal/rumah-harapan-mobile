f298256e786b6933a97037b72a126c1cf80a05f9
zuhalal <zuhalalimulhadi19@gmail.com>
2021-12-20 11:24:17 +0700

    Fetch data to card

--- donasi/lib/screens/donasi_add_form.dart
+++ donasi/lib/screens/donasi_add_form.dart
@@ -17,185 +17,226 @@ class _AddDonasiFormState extends State<AddDonasiForm> {
   String targetFieldsValue = "";
   String tenggatFieldsValue = "";
   String linkFieldsValue = "";
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Padding(padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-            child: Column(
-              children: [
-                Form(
-                    key: _formKey,
-                    child: Column(
-                      children: [
-                        Column(
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text("Tambahkan Donasi"),
+        ),
+        body: SingleChildScrollView(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                child: Column(
+                  children: [
+                    Form(
+                        key: _formKey,
+                        child: Column(
                           children: [
-                            Text("Tambahkan Donasi", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24, color: const Color(0xff59A5D8))),
-                            SizedBox(height: 36,),
+                            Column(
+                              children: [
+                                Text("Tambahkan Donasi",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 24,
+                                        color: const Color(0xff59A5D8))),
+                                SizedBox(
+                                  height: 36,
+                                ),
+                                TextFormField(
+                                  autofocus: true,
+                                  decoration: new InputDecoration(
+                                    hintText: "masukan judul donasi",
+                                    labelText: "Judul",
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            new BorderRadius.circular(5.0)),
+                                  ),
+                                  validator: (value) {
+                                    if (value!.isEmpty) {
+                                      return 'Judul tidak boleh kosong';
+                                    }
+                                    textFieldsValue = value;
+                                    return null;
+                                  },
+                                ),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
                             TextFormField(
                               autofocus: true,
                               decoration: new InputDecoration(
-                                hintText: "masukan judul donasi",
-                                labelText: "Judul",
+                                hintText: "masukan deskripsi donasi",
+                                labelText: "Deskripsi",
                                 border: OutlineInputBorder(
-                                    borderRadius: new BorderRadius.circular(5.0)),
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
                               ),
                               validator: (value) {
                                 if (value!.isEmpty) {
-                                  return 'Judul tidak boleh kosong';
+                                  return 'deskripsi tidak boleh kosong';
                                 }
-                                textFieldsValue = value;
+                                deskripsiFieldsValue = value;
                                 return null;
                               },
                             ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            TextFormField(
+                              autofocus: true,
+                              decoration: new InputDecoration(
+                                hintText: "masukan gambar donasi",
+                                labelText: "Image",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'deskripsi tidak boleh kosong';
+                                }
+                                imageFieldsValue = value;
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            TextFormField(
+                              autofocus: true,
+                              decoration: new InputDecoration(
+                                hintText: "masukan nama penggalang donasi",
+                                labelText: "Penggalang",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'nama penggalang tidak boleh kosong';
+                                }
+                                penggalangFieldsValue = value;
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            TextFormField(
+                              autofocus: true,
+                              decoration: new InputDecoration(
+                                hintText: "masukan nama penerima donasi",
+                                labelText: "Penerima",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'nama penerima tidak boleh kosong';
+                                }
+                                penerimaFieldsValue = value;
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            TextFormField(
+                              autofocus: true,
+                              decoration: new InputDecoration(
+                                hintText: "masukan target donasi",
+                                labelText: "Target",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'target tidak boleh kosong';
+                                }
+                                targetFieldsValue = value;
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            TextFormField(
+                              autofocus: true,
+                              decoration: new InputDecoration(
+                                hintText: "masukan tanggal donasi",
+                                labelText: "Tenggat waktu",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Tenggat waktu tidak boleh kosong';
+                                }
+                                tenggatFieldsValue = value;
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            TextFormField(
+                              autofocus: true,
+                              decoration: new InputDecoration(
+                                hintText: "masukan link donasi",
+                                labelText: "Link Donasi",
+                                border: OutlineInputBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(5.0)),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Link donasi tidak boleh kosong';
+                                }
+                                linkFieldsValue = value;
+                                return null;
+                              },
+                            ),
+                            SizedBox(
+                              height: 36,
+                            ),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  textStyle: const TextStyle(fontSize: 20),
+                                  onPrimary: Colors.white,
+                                  primary: const Color(0xff023E8A),
+                                  side: BorderSide(
+                                      width: 2, color: const Color(0xff023E8A)),
+                                  padding: EdgeInsets.only(
+                                      left: 12, right: 12, top: 8, bottom: 8),
+                                  shape: new RoundedRectangleBorder(
+                                      borderRadius:
+                                          new BorderRadius.circular(8.0))),
+                              onPressed: () {
+                                if (_formKey.currentState!.validate()) {
+                                  print(textFieldsValue);
+                                  print(deskripsiFieldsValue);
+                                  print(imageFieldsValue);
+                                  print(penggalangFieldsValue);
+                                  print(penerimaFieldsValue);
+                                  print(targetFieldsValue);
+                                  print(tenggatFieldsValue);
+                                  print(linkFieldsValue);
+                                }
+                              },
+                              child: const Text('Donasi Sekarang'),
+                            ),
                           ],
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan deskripsi donasi",
-                            labelText: "Deskripsi",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'deskripsi tidak boleh kosong';
-                            }
-                            deskripsiFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan gambar donasi",
-                            labelText: "Image",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'deskripsi tidak boleh kosong';
-                            }
-                            imageFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan nama penggalang donasi",
-                            labelText: "Penggalang",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'nama penggalang tidak boleh kosong';
-                            }
-                            penggalangFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan nama penerima donasi",
-                            labelText: "Penerima",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'nama penerima tidak boleh kosong';
-                            }
-                            penerimaFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan target donasi",
-                            labelText: "Target",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'target tidak boleh kosong';
-                            }
-                            targetFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan tanggal donasi",
-                            labelText: "Tenggat waktu",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Tenggat waktu tidak boleh kosong';
-                            }
-                            tenggatFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        TextFormField(
-                          autofocus: true,
-                          decoration: new InputDecoration(
-                            hintText: "masukan link donasi",
-                            labelText: "Link Donasi",
-                            border: OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(5.0)),
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Link donasi tidak boleh kosong';
-                            }
-                            linkFieldsValue = value;
-                            return null;
-                          },
-                        ),
-                        SizedBox(height: 36,),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20),
-                              onPrimary: Colors.white,
-                              primary: const Color(0xff023E8A),
-                              side: BorderSide(width: 2,color: const Color(0xff023E8A)),
-                              padding: EdgeInsets.only(left: 12, right: 12, top: 8, bottom: 8),
-                              shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              print(textFieldsValue);
-                              print(deskripsiFieldsValue);
-                              print(imageFieldsValue);
-                              print(penggalangFieldsValue);
-                              print(penerimaFieldsValue);
-                              print(targetFieldsValue);
-                              print(tenggatFieldsValue);
-                              print(linkFieldsValue);
-                            }
-                          },
-                          child: const Text('Donasi Sekarang'),
-                        ),
-                      ],
-                    )
-                )
-              ],
-            )
-        )
-    );
+                        ))
+                  ],
+                ))));
   }
 }
--- donasi/lib/widgets/card_carousel.dart
+++ donasi/lib/widgets/card_carousel.dart
@@ -1,7 +1,11 @@
+import 'package:donasi/models/model.dart';
 import 'package:flutter/material.dart';
 
 class CardCarousel extends StatefulWidget {
-  const CardCarousel({Key? key}) : super(key: key);
+  const CardCarousel({Key? key, required this.data, required this.isUser})
+      : super(key: key);
+  final AllDonasi data;
+  final bool isUser;
 
   @override
   _CardCarouselState createState() => _CardCarouselState();
@@ -13,57 +17,97 @@ class _CardCarouselState extends State<CardCarousel> {
     return Card(
       child: Column(
         mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Image.asset('assets/images/donasi/donate.jpg'),
-          Container(
-            padding: EdgeInsets.all(12),
-            child: Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis enim vitae arcu lacinia pretium. Cras nisi nulla, posuere a sem sed, blandit vulputate lectus. Duis faucibus odio at dui porttitor tempor.",
-                textAlign: TextAlign.justify),
-          ),
+          Column(children: [
+            Image.network(
+                'https://res.cloudinary.com/dnrjqdl6n/' +
+                    widget.data.fields.image,
+                height: 160),
+            Container(
+              padding: EdgeInsets.all(12),
+              child: Text(widget.data.fields.deskripsi,
+                  textAlign: TextAlign.justify),
+            ),
+          ]),
           Padding(
             padding: EdgeInsets.all(12),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20),
-                    onPrimary: Colors.white,
-                    primary: const Color(0xff023E8A),
-                    side: BorderSide(width: 2,color: const Color(0xff023E8A)),
-                    padding: EdgeInsets.only(left: 12, right: 12, top: 8, bottom: 8),
-                    shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0)),),
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/donasi-detail');
-                  },
-                  child: const Text('Lihat'),
-                ),
-                SizedBox(height: 4),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20),
-                      primary: Colors.white,
-                      side: BorderSide(width: 2,color: const Color(0xff023E8A)),
-                      padding: EdgeInsets.only(left: 12, right: 12, top: 8, bottom: 8),
-                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))),
-                  onPressed: () {},
-                  child: const Text('Edit',
-                      style: TextStyle(color: Colors.black)
-                  ),
-                ),
-                SizedBox(height: 4),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20),
-                      onPrimary: Colors.white,
-                      primary: const Color(0xffEB5757),
-                      side: BorderSide(width: 2,color: const Color(0xffEB5757)),
-                      padding: EdgeInsets.only(left: 12, right: 12, top: 8, bottom: 8),
-                      shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(8.0))),
-                  onPressed: () {
-
-                  },
-                  child: const Text('Hapus',
-                  ),
-                ),
-              ],
+              mainAxisAlignment: widget.isUser
+                  ? MainAxisAlignment.spaceBetween
+                  : MainAxisAlignment.center,
+              children: widget.isUser
+                  ? [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20),
+                          onPrimary: Colors.white,
+                          primary: const Color(0xff023E8A),
+                          side: BorderSide(
+                              width: 2, color: const Color(0xff023E8A)),
+                          padding: EdgeInsets.only(
+                              left: 12, right: 12, top: 8, bottom: 8),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(8.0)),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/donasi-detail');
+                        },
+                        child: const Text('Lihat'),
+                      ),
+                      SizedBox(height: 4),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                            primary: Colors.white,
+                            side: BorderSide(
+                                width: 2, color: const Color(0xff023E8A)),
+                            padding: EdgeInsets.only(
+                                left: 12, right: 12, top: 8, bottom: 8),
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(8.0))),
+                        onPressed: () {},
+                        child: const Text('Edit',
+                            style: TextStyle(color: Colors.black)),
+                      ),
+                      SizedBox(height: 4),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(fontSize: 20),
+                            onPrimary: Colors.white,
+                            primary: const Color(0xffEB5757),
+                            side: BorderSide(
+                                width: 2, color: const Color(0xffEB5757)),
+                            padding: EdgeInsets.only(
+                                left: 12, right: 12, top: 8, bottom: 8),
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(8.0))),
+                        onPressed: () {},
+                        child: const Text(
+                          'Hapus',
+                        ),
+                      ),
+                    ]
+                  : [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20),
+                          onPrimary: Colors.white,
+                          primary: const Color(0xff023E8A),
+                          side: BorderSide(
+                              width: 2, color: const Color(0xff023E8A)),
+                          padding: EdgeInsets.only(
+                              left: 12, right: 12, top: 8, bottom: 8),
+                          shape: new RoundedRectangleBorder(
+                              borderRadius: new BorderRadius.circular(8.0)),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/donasi-detail');
+                        },
+                        child: const Text('Lihat'),
+                      ),
+                      SizedBox(height: 4),
+                    ],
             ),
           )
         ],
