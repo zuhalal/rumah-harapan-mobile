9b0d77e05a1ee46a09b0e4fe3bb50d3311abea2e
zuhalal <zuhalalimulhadi19@gmail.com>
2021-12-20 11:28:44 +0700

    Refactor main dart

--- donasi/lib/screens/donasi_home.dart
+++ donasi/lib/screens/donasi_home.dart
@@ -4,6 +4,7 @@ import 'package:donasi/models/model.dart';
 import 'package:flutter/material.dart';
 import 'package:donasi/widgets/card_carousel.dart';
 import 'package:http/http.dart' as http;
+import 'package:rumah_harapan/widgets/drawer.dart';
 
 class DonasiHome extends StatefulWidget {
   const DonasiHome({Key? key}) : super(key: key);
@@ -51,165 +52,173 @@ class _DonasiHomeState extends State<DonasiHome> {
   }
 
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-        child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-              color: const Color(0xffade8f4),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Image.asset('assets/images/donasi/3724843.png'),
-                    Text("Donasi",
-                        textAlign: TextAlign.left,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 32,
-                            color: const Color(0xff59A5D8))),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    Container(
-                      child: Text(
-                          "Mari kita ringankan beban sesama #TemanHarapan di tengah pandemi Covid-19 ini. Kamu dapat membantu dengan berdonasi sesuai dengan keinginan dan keikhlasan kamu. "
-                          "Setiap uang yang kamu keluarkan dapat menjadi pintu rezeki bagi mereka yang membutuhkan. ",
-                          textAlign: TextAlign.justify,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Donasi"),
+      ),
+      drawer: DrawerScreen(),
+      body: Center(
+          child: SingleChildScrollView(
+              child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+                color: const Color(0xffade8f4),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Image.asset('assets/images/donasi/3724843.png'),
+                      Text("Donasi",
+                          textAlign: TextAlign.left,
                           style: TextStyle(
-                              fontWeight: FontWeight.normal, fontSize: 16)),
-                    ),
-                    SizedBox(height: 24),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            textStyle: const TextStyle(fontSize: 20),
-                            primary: Colors.black12,
-                            onPrimary: Colors.white,
-                            side: BorderSide(
-                                width: 2, color: const Color(0xff023E8A)),
-                            padding: EdgeInsets.only(
-                                left: 12, right: 12, top: 8, bottom: 8),
-                            shape: new RoundedRectangleBorder(
-                                borderRadius: new BorderRadius.circular(8.0)),
-                          ),
-                          onPressed: () {},
-                          child: const Text('Lihat Donasi Saya'),
-                        ),
-                        SizedBox(height: 4),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 32,
+                              color: const Color(0xff59A5D8))),
+                      SizedBox(
+                        height: 24,
+                      ),
+                      Container(
+                        child: Text(
+                            "Mari kita ringankan beban sesama #TemanHarapan di tengah pandemi Covid-19 ini. Kamu dapat membantu dengan berdonasi sesuai dengan keinginan dan keikhlasan kamu. "
+                            "Setiap uang yang kamu keluarkan dapat menjadi pintu rezeki bagi mereka yang membutuhkan. ",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                                fontWeight: FontWeight.normal, fontSize: 16)),
+                      ),
+                      SizedBox(height: 24),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
                               textStyle: const TextStyle(fontSize: 20),
+                              primary: Colors.black12,
                               onPrimary: Colors.white,
-                              primary: const Color(0xff023E8A),
                               side: BorderSide(
                                   width: 2, color: const Color(0xff023E8A)),
                               padding: EdgeInsets.only(
                                   left: 12, right: 12, top: 8, bottom: 8),
                               shape: new RoundedRectangleBorder(
-                                  borderRadius:
-                                      new BorderRadius.circular(8.0))),
-                          onPressed: () {
-                            fetchData();
-                          },
-                          child: const Text('Donasi Sekarang'),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              )),
-        ),
-        SizedBox(height: 24),
-        Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text("Mari Bantu Sesama #TemanHarapan",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 32,
-                      color: const Color(0xff59A5D8))),
-              SizedBox(height: 24),
-              FutureBuilder(
-                  future: fetchData(),
-                  builder: (context, AsyncSnapshot snapshot) {
-                    if (snapshot.data == null) {
-                      return Container(
-                        child: Center(
-                            child: Text(
-                          "Loading...",
-                        )),
-                      );
-                    } else {
-                      return CarouselSlider(
-                        options: CarouselOptions(height: 388.0),
-                        items: extractedData.map((data) {
-                          return Builder(
-                            builder: (BuildContext context) {
-                              return Container(
-                                  margin: EdgeInsets.symmetric(horizontal: 4.0),
-                                  child:
-                                      CardCarousel(data: data, isUser: false));
+                                  borderRadius: new BorderRadius.circular(8.0)),
+                            ),
+                            onPressed: () {},
+                            child: const Text('Lihat Donasi Saya'),
+                          ),
+                          SizedBox(height: 4),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(fontSize: 20),
+                                onPrimary: Colors.white,
+                                primary: const Color(0xff023E8A),
+                                side: BorderSide(
+                                    width: 2, color: const Color(0xff023E8A)),
+                                padding: EdgeInsets.only(
+                                    left: 12, right: 12, top: 8, bottom: 8),
+                                shape: new RoundedRectangleBorder(
+                                    borderRadius:
+                                        new BorderRadius.circular(8.0))),
+                            onPressed: () {
+                              fetchData();
                             },
-                          );
-                        }).toList(),
-                      );
-                    }
-                  }),
-            ],
+                            child: const Text('Donasi Sekarang'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
           ),
-        ),
-        SizedBox(
-          width: double.infinity,
-          child: Container(
-              color: const Color(0xffade8f4),
-              child: Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("+ Tambah Donasi",
-                            textAlign: TextAlign.right,
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16,
-                                color: const Color(0xff59A5D8))),
-                      ],
-                    ),
-                    SizedBox(height: 24),
-                    Text("Donasi Saya",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 32,
-                            color: const Color(0xff59A5D8))),
-                    SizedBox(height: 24),
-                    // CarouselSlider(
-                    //   options: CarouselOptions(height: 388.0),
-                    //   items: [1,2].map((i) {
-                    //     return Builder(
-                    //       builder: (BuildContext context) {
-                    //         return Container(
-                    //             margin: EdgeInsets.symmetric(horizontal: 4.0),
-                    //             child: CardCarousel()
-                    //         );
-                    //       },
-                    //     );
-                    //   }).toList(),
-                    // ),
-                  ],
-                ),
-              )),
-        ),
-      ],
-    ));
+          SizedBox(height: 24),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text("Mari Bantu Sesama #TemanHarapan",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
+                        color: const Color(0xff59A5D8))),
+                SizedBox(height: 24),
+                FutureBuilder(
+                    future: fetchData(),
+                    builder: (context, AsyncSnapshot snapshot) {
+                      if (snapshot.data == null) {
+                        return Container(
+                          child: Center(
+                              child: Text(
+                            "Loading...",
+                          )),
+                        );
+                      } else {
+                        return CarouselSlider(
+                          options: CarouselOptions(height: 388.0),
+                          items: extractedData.map((data) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Container(
+                                    margin:
+                                        EdgeInsets.symmetric(horizontal: 4.0),
+                                    child: CardCarousel(
+                                        data: data, isUser: false));
+                              },
+                            );
+                          }).toList(),
+                        );
+                      }
+                    }),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Container(
+                color: const Color(0xffade8f4),
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text("+ Tambah Donasi",
+                              textAlign: TextAlign.right,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: const Color(0xff59A5D8))),
+                        ],
+                      ),
+                      SizedBox(height: 24),
+                      Text("Donasi Saya",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 32,
+                              color: const Color(0xff59A5D8))),
+                      SizedBox(height: 24),
+                      // CarouselSlider(
+                      //   options: CarouselOptions(height: 388.0),
+                      //   items: [1,2].map((i) {
+                      //     return Builder(
+                      //       builder: (BuildContext context) {
+                      //         return Container(
+                      //             margin: EdgeInsets.symmetric(horizontal: 4.0),
+                      //             child: CardCarousel()
+                      //         );
+                      //       },
+                      //     );
+                      //   }).toList(),
+                      // ),
+                    ],
+                  ),
+                )),
+          ),
+        ],
+      ))),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:donasi/screens/donasi_detail.dart';
 import 'package:donasi/screens/donasi_home.dart';
 import 'package:flutter/material.dart';
-import 'package:rumah_harapan/widgets/drawer.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,47 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Rumah Harapan🏠'),
-      routes: <String, WidgetBuilder> {
-        '/donasi':(context)=>DonasiHome(),
-        '/donasi-detail': (context)=>DonasiDetail()
+      home: DonasiHome(),
+      routes: <String, WidgetBuilder>{
+        '/donasi': (context) => DonasiHome(),
+        '/donasi-detail': (context) => DonasiDetail()
       },
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      drawer: DrawerScreen(),
-      body: Center(
-        child: DonasiHome()
-      ),
     );
   }
 }
